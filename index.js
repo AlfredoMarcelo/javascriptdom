f62f6f6bae50cd8cf2226fd471d7db430142376d
alfredo <alfredo.supanta.26@gmail.com>
2022-01-18 21:16:01 -0300

    practica con fragment

--- index.js
+++ index.js
@@ -2,18 +2,53 @@ const listaDeNombre = document.getElementById("listaDeNombre");
 
 const paises = ["Chile","Perú","Venezuela"];
 
-//-----------evitar reflow con fragment-----------
-
 
 const fragment = document.createDocumentFragment()
 
 paises.forEach((pais)=>{
+    const li = document.createElement('li');
+    li.className = "List"
+
+    const b = document.createElement('b');
+    b.textContent = "País: "
+
+    const span = document.createElement('span');
+    span.className = "text-warning"
+    span.textContent = pais
+
+    li.appendChild(b);
+    li.appendChild(span);
+
+    fragment.appendChild(li);
+})
+listaDeNombre.appendChild(fragment)
+
+
+
+
+
+
+
+
+
+
+
+
+//-----------evitar reflow con fragment-----------
+
+
+/* const fragment = document.createDocumentFragment()
+
+paises.forEach((pais)=>{
     const li = document.createElement("li");
     li.textContent = pais;
-    listaDeNombre.appendChild(li)
+    fragment.appendChild(li)
 })// se recorre el array pero fuera del dom y en la linea 16 se añade recien al dom, una vez que ya esta listo
 
 listaDeNombre.appendChild(fragment)
+ */
+
+
 
 //--------------------------------------
 // ejemplo de innerhtml (no utilizar )
